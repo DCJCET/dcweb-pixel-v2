3775e649cad7e34a4a19b8f36758ad08ddb9e99a
Nischal Jagadish Mudennavar <nischalmudennavar@gmail.com>
2021-11-23 03:48:34 +0530

    getting the design funky

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 // js/script.js
 
-// var title = document.getElementsByClassName("title")[0];
-// var title_text = title.innerHTML;
-// var title_text_length = title_text.length;
-// var words = title_text.split(" ");
+// GETrANDOM COLOR FUNCTION
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+// map through ever word and change its color
+var title = document.getElementsByClassName("bg-text")[0];
+var title_text = title.innerHTML;
+var title_text_length = title_text.length;
+var words = title_text.split(" ");
+function animatebg() {
+    
+    for (var i = 0; i < words.length; i++) {
+      var word = words[i];
+      var word_length = word.length;
+      var word_color = getRandomColor();
+      var word_html =
+        "<span style='color:" + word_color + "'>" + word + "</span>";
+      title.innerHTML = title.innerHTML.replace(word, word_html);
+    }
 
-// console.log(words);
+}
+window.setInterval(animatebg, 100);
+console.log(words);
