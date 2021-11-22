c6e8d2210239628acf8ba40ee8a9ea1b2903620a
Nischal Jagadish Mudennavar <nischalmudennavar@gmail.com>
2021-11-23 03:07:10 +0530

    home section done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// js/script.js
+
+// var title = document.getElementsByClassName("title")[0];
+// var title_text = title.innerHTML;
+// var title_text_length = title_text.length;
+// var words = title_text.split(" ");
+
+// console.log(words);
